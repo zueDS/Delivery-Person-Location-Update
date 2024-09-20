73c93117ff1cea6f1eedab73b99fb0d62129a17d
ShuttlerBad <dileepasudara@gmail.com>
2024-09-20 11:00:17 +0530

    loading assigned orderz

---
+++ app/src/main/java/com/example/iconichandcrafters/LoadAssignedOrders.java
@@ -0,0 +1,22 @@
+package com.example.iconichandcrafters;
+
+public class LoadAssignedOrders {
+    String orderidload;
+    String datez;
+    String timez;
+
+    LoadAssignedOrders(String orderidload,String datez,String timez){
+
+        orderidload = this.orderidload;
+        datez = this.datez;
+        timez = this.timez;
+        
+
+    }
+
+    LoadAssignedOrders(){
+
+
+    }
+
+}
